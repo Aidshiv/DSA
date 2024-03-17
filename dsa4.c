c47db57f3993d12c8b793dc8816565cf7f77955f
Aidshiv <163630330+Aidshiv@users.noreply.github.com>
2024-03-17 17:10:36 +0530

    dsa4.c

--- dsa4.c
+++ dsa4.c
@@ -2,23 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 #define MAX_STACK_SIZE 100
-// Structure to represent a stack
 struct Stack
 {
     int top;
     char items[MAX_STACK_SIZE];
 };
-// Function to initialize the stack
 void initialize(struct Stack *stack)
 {
     stack->top = -1;
 }
-// Function to check if the stack is empty
 int isEmpty(struct Stack *stack)
 {
     return stack->top == -1;
 }
-// Function to push an element onto the stack
 void push(struct Stack *stack, char item)
 {
     if (stack->top == MAX_STACK_SIZE - 1)
@@ -28,7 +24,6 @@ void push(struct Stack *stack, char item)
     }
     stack->items[++stack->top] = item;
 }
-// Function to pop an element from the stack
 char pop(struct Stack *stack)
 {
     if (isEmpty(stack))
@@ -38,12 +33,10 @@ char pop(struct Stack *stack)
     }
     return stack->items[stack->top--];
 }
-// Function to check if a character is an operator
 int isOperator(char c)
 {
     return (c == '+' || c == '-' || c == '*' || c == '/' || c == '%' || c == '^');
 }
-// Function to get the precedence of an operator
 int getPrecedence(char c)
 {
     if (c == '^')
@@ -54,7 +47,6 @@ int getPrecedence(char c)
         return 1;
     return 0;
 }
-// Function to convert an infix expression to a postfix expression
 void infixToPostfix(char *infix, char *postfix)
 {
     struct Stack stack;
@@ -80,7 +72,7 @@ void infixToPostfix(char *infix, char *postfix)
             }
             if (!isEmpty(&stack) && stack.items[stack.top] == '(')
             {
-                pop(&stack); // Pop the opening parenthesis
+                pop(&stack);
             }
         }
         else if (isOperator(current))
@@ -100,6 +92,13 @@ void infixToPostfix(char *infix, char *postfix)
     }
     postfix[j] = '\0';
 }
+
+// output
+// Enter an infix expression: (A+B*C) 
+// Postfix expression: ABC*+
+// Enter an infix expression: (A/B)+(C*D)
+// Postfix expression: AB/CD*+
+    
 int main()
 {
     char infix[100];
